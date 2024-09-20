a8a5fa426d9cb46da49a300ed1eb446a4fd050d2
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-09-20 16:06:16 +0530

    initialize

--- src/main/java/org/icet/equipmentcrm/Main.java
+++ src/main/java/org/icet/equipmentcrm/Main.java
@@ -1,6 +1,12 @@
 package org.icet.equipmentcrm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        SpringApplication.run(Main.class);
     }
 }
