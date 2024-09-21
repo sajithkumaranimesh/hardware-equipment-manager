21958bda3b7d3cace10dbe9687f39d1b917b51a5
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-09-21 21:49:56 +0530

    create the application.yml property file
    Add the MySQL connector and connect the controller to the service.

--- src/main/java/org/icet/equipmentcrm/controller/PersonController.java
+++ src/main/java/org/icet/equipmentcrm/controller/PersonController.java
@@ -1,17 +1,22 @@
 package org.icet.equipmentcrm.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.icet.equipmentcrm.dto.Person;
+import org.icet.equipmentcrm.service.PersonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/person")
+@RequiredArgsConstructor
 public class PersonController {
+
+    private final PersonService service;
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public void persist(@RequestBody Person person){
-
+        service.persist(person);
     }
 }
