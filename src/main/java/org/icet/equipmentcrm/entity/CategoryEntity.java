ba127c724d24ab754fbd647925df93775aeca763
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2024-09-23 16:06:33 +0530

    create category entity

---
+++ src/main/java/org/icet/equipmentcrm/entity/CategoryEntity.java
@@ -0,0 +1,24 @@
+package org.icet.equipmentcrm.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Date registerdDate;
+}
